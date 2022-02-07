7e428c4aa3c4f3946e19ac0d2db00071aaae1edf
Murilo Oliveira <murilomartins1404@gmail.com>
2022-02-07 10:59:49 -0300

    atualização

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,18 +1,19 @@
-function carregar(){
+function carregar () {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
-    
-    if(hora >= 8 && hora < 12){
+    if (hora >= 8 && hora < 12){
         //BOM DIA!
-        img.src = '../imagens/fotomanha.png'
-    } else if(hora >= 12 && hora < 18){
+        document.body.style.background = '#ffb967'
+    } else if(hora >= 12 && hora <= 18){
         //BOA TARDE!
-        img.src = '../imagens/fototarde.png'
-    }else{
-        //BOA NOITE!
-        img.src = '../imagens/fotonoite.png'
+        img.src='imagens/fototarde.png'
+        document.body.style.background = '#f16500'
+    } else {
+        //BOA NOITE !
+        img.src = 'imagens/fotonoite.png'
+        document.body.style.background = '#423457'
     }
 }
